c04dca06747a74c7090b600da7984b6b2df1decd
Alberto Ayala <aayala@voyager>
2023-11-16 14:36:27 -0800

    with the flag -w we support csv files with headers

--- csv2json.py
+++ csv2json.py
@@ -1,4 +1,4 @@
-#!python3 
+#!python3
 
 import argparse
 import json
@@ -9,13 +9,15 @@ def main():
     parser = argparse.ArgumentParser(description="csv2json")
     parser.add_argument('-f', '--file', required=True, help="csv file to parse")
     parser.add_argument('-o', '--output', required=True, help="json file")
+    parser.add_argument('-w', '--with-header', action="store_true", help="csv without header")
     args = parser.parse_args()
-    
+
     input  = args.file
     output = args.output
-    
+    header = args.with_header
+
     obj_lst = []
- 
+
     with open(input, 'r') as csv_file:
 
         line_count = 0
@@ -23,36 +25,36 @@ def main():
 
         for line in csv_file:
 
-            if line_count == 0:
+            if line_count == 0 and header:
                 if '"' in line:
                     line2 = re.sub(r'("[^"]*")', lambda x: x.group(0).replace(',', ';'), line )
                     fields = line2.strip().split(",")
                 else:
                     fields = line.strip().split(",")
-                rows_count = len(fields)
+                rows = fields
 
-            if line_count > 0:
+            if line_count > 0 or not header:
                 if '"' in line:
                     line2 = re.sub(r'("[^"]*")', lambda x: x.group(0).replace(',', ';'), line )
                     rows = line2.strip().split(",")
-                else:    
+                else:
                     rows = line.strip().split(",")
-                if fields:
+                if header:
                     obj_lst.append(dict(map(lambda i,j : (i,j), fields,rows)))
                 else:
                     obj_lst.append(",".join(rows))
 
-                if rows_count != len(rows):
-                   sys.exit('file {}, line {}: {}'.format(input, line_count + 1 , "The number of fields does not match")) 
-                rows_count = len(rows)
+            if rows_count != len(rows) and rows_count > 0:
+                sys.exit('file {}, line {}: {}'.format(input, line_count + 1 , "number of fields does not match previous line"))
 
             line_count +=1
-        
+            rows_count = len(rows)
+
     if line_count == 1:
-        sys.exit('file {}, line {}: {}'.format(input, line_count + 1 , "there are only 1 row in this file")) 
-    
+        sys.exit('file {}, line {}: {}'.format(input, line_count + 1 , "there are only 1 row in this file"))
+
     with open(output, 'w') as out_file:
         out_file.write(json.dumps(obj_lst))
-    
+
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
-examples csv files come from:
+# csv2json
+
+![DEMO!](demo.png)
+
+Examples csv files come from:
   - https://catalog.data.gov/dataset/electric-vehicle-population-data
   - https://wsform.com/knowledgebase/sample-csv-files
+  - https://en.wikipedia.org/wiki/Comma-separated_values
 
 Current issues:
- - the csv2json does not work properly without header line that define the fields
  - need to write unittest
